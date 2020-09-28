b7cceba938d2a731f6baa5cf6ceb224743f50e04
Grant Eadie <grantleadie@gmail.com>
2020-09-27 19:22:08 -0700

    add order class.

--- BreadPastry/Models/BreadPastry.cs
+++ BreadPastry/Models/BreadPastry.cs
@@ -4,25 +4,31 @@ using System.Linq;
 
 namespace BreadPastry.Models
 {
-  public class Bread
-  {
-    public int BreadPrices{ get; set; }
-
-    public Bread (int prices)
-    {
-      BreadPrices = prices;
-    }
-    
-  }
-  public class Pastry
+  public class Order
   {
 
-    public int PastryPrices{ get; set; }
-
-    public Pastry (int prices)
+    public class Bread
     {
-      PastryPrices = prices;
-    }
+      public int BreadPrices { get; set; }
 
+      public Bread(int prices)
+      {
+        BreadPrices = prices;
+        _order.Add(this);
+      }
+
+
+    }
+    public class Pastry
+    {
+
+      public int PastryPrices { get; set; }
+
+      public Pastry(int prices)
+      {
+        PastryPrices = prices;
+      }
+
+    }
   }
 }
